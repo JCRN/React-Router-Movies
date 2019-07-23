8ceedd571344630989a6fecbd6b44c5b6fa5c62e
JCRN <clemons.jason@gmail.com>
2019-07-23 14:17:48 -0700

    added save movie funtionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import Movie from './Movies/Movie';
-import MovieList from './Movies/MovieList';
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react"
+import { Route } from "react-router-dom"
+import Movie from "./Movies/Movie"
+import MovieList from "./Movies/MovieList"
+import SavedList from "./Movies/SavedList"
 
 const App = () => {
   const [savedList, setSavedList] = useState([])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie])
-  };
+  }
 
   return (
     <div>
       <SavedList list={savedList} />
-      <Route path="/" exact component={MovieList} />
+      <Route path='/' exact component={MovieList} />
       <Route
-        path="/movies/:id"
+        path='/movies/:id'
         render={props => {
-          return <Movie {...props} />
+          return <Movie {...props} addToSavedList={addToSavedList} />
         }}
       />
     </div>
   )
-};
+}
 
 export default App
